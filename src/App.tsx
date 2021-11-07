b28cc8177f88608f08cc31a00b2b775547563e57
Felipe <contato@eufelipe.com>
2021-11-07 17:11:19 -0300

    feat: add React Navigation support

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,51 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Button, Text} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-const App = () => {
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Icon name="rocket" size={48} color="#900" />
+
+      <Text style={{color: 'red', fontSize: 32, fontFamily: 'Poppins-Regular'}}>
+        HomeScreen
+      </Text>
+
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+};
+
+const DetailsScreen = ({navigation}) => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text style={{color: 'red', fontSize: 32, fontFamily: 'Poppins-Regular'}}>
-        Log Finance
+        DetailsScreen
       </Text>
       <Icon name="rocket" size={48} color="#900" />
+
+      <Button title="Go to Details... again" onPress={() => navigation.pop()} />
     </View>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
