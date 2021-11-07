9b273eee2ce0ff9505b70459922b9481227dbfff
Felipe <contato@eufelipe.com>
2021-11-07 16:48:57 -0300

    feat: add Poppins font support

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 
-import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{color: 'red', fontSize: 32, fontFamily: 'Poppins-Regular'}}>
+        Log Finance
+      </Text>
       <Icon name="rocket" size={48} color="#900" />
     </View>
   );
