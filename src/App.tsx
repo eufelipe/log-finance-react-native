1f627af0bc82c22bb048efc5e2e28f7d73e998d9
Felipe <contato@eufelipe.com>
2021-11-07 15:59:41 -0300

    feat: add icon support using react-native-vector-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {View} from 'react-native';
 
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
 const App = () => {
-  return <View />;
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Icon name="rocket" size={48} color="#900" />
+    </View>
+  );
 };
 
 export default App;
