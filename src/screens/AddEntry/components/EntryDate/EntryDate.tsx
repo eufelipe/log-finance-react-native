790b5562e85ba13d442eb53ea4be12058544849c
Felipe <contato@eufelipe.com>
2021-11-16 18:46:32 -0300

    feat: add date selector

--- src/screens/AddEntry/components/EntryDate/EntryDate.tsx
+++ src/screens/AddEntry/components/EntryDate/EntryDate.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {isToday, format} from 'date-fns';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 import Row from '../Row';
 
-import {Label, Title} from './styles';
+import {Label, Title, Touchable} from './styles';
+import {useEntry} from 'hooks/useEntry';
 
 const EntryDate = (): JSX.Element => {
   const {t} = useTranslation('add');
+  const {date = new Date(), setDate} = useEntry();
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
-  const date = new Date();
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (dateSelected: Date) => {
+    setDate(dateSelected);
+    hideDatePicker();
+  };
+
   const today = isToday(date);
 
   const formattedDate = format(
@@ -20,10 +36,21 @@ const EntryDate = (): JSX.Element => {
   );
 
   return (
-    <Row icon="event">
-      <Label>{t('date')}</Label>
-      <Title>{formattedDate}</Title>
-    </Row>
+    <>
+      <Touchable onPress={showDatePicker}>
+        <Row icon="event">
+          <Label>{t('date')}</Label>
+          <Title>{formattedDate}</Title>
+        </Row>
+      </Touchable>
+      <DateTimePickerModal
+        date={date}
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
+    </>
   );
 };
 
